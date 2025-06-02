e51915f5c4f8ed5f594a66cabb8ca0c68fbb40ef
Thiago Bignotto <bignotto@gmail.com>
2025-06-02 18:26:28 -0300

    :floppy_disk: save commit

--- produção de urnas por dia.sql
+++ produção de urnas por dia.sql
@@ -1,12 +1,19 @@
+with date_series as (
+    select generate_series(
+        '2025-05-01'::date,
+        '2025-05-31'::date,
+        '1 day'::interval
+    )::date as date
+)
 select
-    R0.ordemreporte_data_reporte,
-    sum(R0.ordemreporte_qtde_reportada)
-from public.pw_ordem_reporte R0
-    inner join public.pw_ordem R1 on R1.ordem_codigo_pk = R0.ordemreporte_ordem_codigo_fk
-    inner join public.produto P on P.produto = R1.ordem_produto_codigo_fk
-where EXTRACT(MONTH from R0.ordemreporte_data_reporte) = 4
-    and P.grupo = 1
+    d.date as ordemreporte_data_reporte,
+    coalesce(sum(R0.ordemreporte_qtde_reportada), 0) as ordemreporte_qtde_reportada
+from date_series d
+    left join public.pw_ordem_reporte R0 on d.date = R0.ordemreporte_data_reporte
+    left join (
+        select * from public.pw_ordem R1 on R1.ordem_codigo_pk = R0.ordemreporte_ordem_codigo_fk
+        inner join public.produto P on P.produto = R1.ordem_produto_codigo_fk
+        where P.grupo = 1
+    ) T0 on d.date = T0.ordemreporte_data_reporte
 
-group by R0.ordemreporte_data_reporte
-
-LIMIT 1000
+group by d.date
---
+++ reporte urnas dia.sql
@@ -0,0 +1,23 @@
+with date_series as (
+    select generate_series(
+        '2025-05-01'::date,
+        '2025-05-31'::date,
+        '1 day'::interval
+    )::date as date
+)
+select
+    d.date as serie_date,
+    coalesce(sum(R0.qtde_reportada), 0) as qtde_reportada
+from date_series d
+    left join (
+        select
+            R0.ordemreporte_data_reporte as data_reporte,
+            P.produto as produto,
+            R0.ordemreporte_qtde_reportada as qtde_reportada
+        from public.pw_ordem_reporte R0
+            inner join public.pw_ordem R1 on R1.ordem_codigo_pk = R0.ordemreporte_ordem_codigo_fk
+            inner join public.produto P on P.produto = R1.ordem_produto_codigo_fk
+                and P.grupo = 1
+    ) R0 on R0.data_reporte = d.date
+group by d.date
+order by d.date;
